76b50645f7f0e9c548d62cf3971ca57b076f87ac
sqrtofsaturn <sqrtofsaturn@gmail.com>
2017-09-29 19:02:08 -0700

    More stuff

--- command.js
+++ command.js
@@ -1,29 +1,111 @@
 #!/usr/bin/env node
 
+const identity = require('lodash/fp/identity')
+const minBy = require('lodash/fp/minBy')
+const isNumber = require('lodash/fp/isNumber')
+const size = require('lodash/fp/size')
+const OctoDash = require('octodash')
+const ThinRedLine = require('thin-red-line')
+
+const median = require('./lib/median')
 const Server = require('./lib/Server')
-const XboxController = require('./lib/XboxController')
+const SnesController = require('./lib/SnesController')
+
+const min = minBy(identity)
+
+const OPTIONS = [{
+  names: ['path'],
+  type: 'string',
+  help: 'Path of the Snes controller to connect to. Will default to first available',
+}, {
+  names: ['port', 'p'],
+  type: 'integer',
+  help: 'Port to expose the Websocket on',
+  env: 'PORT',
+  default: 3000,
+}]
 
 class Command {
-  constructor() {
-    this.server = new Server({ port: 3000 })
-    this.xboxController = new XboxController()
+  constructor({ argv }) {
+    const { path, port } = new OctoDash({ argv, cliOptions: OPTIONS }).parseOptions()
+    this.server = new Server({ port })
+    this.snesController = new SnesController({ path })
+    this.go = true
+    this.distances = []
+    this.direction = 0
+    this.redLine = new ThinRedLine({imageStreamUrl: 'http://bill.local:8081'})
   }
 
   run() {
+    this.redLine.do()
     this.server.listen((error, address) => {
       if (error) throw error
       console.log(`Listening on: ${address}`)
     })
-    this.xboxController.on('sticks', ({ leftY, rightY }) => {
-      let right = (Math.abs(leftY) < 20) ? 0 : (2 * leftY)
-      let left = (Math.abs(rightY) < 20) ? 0 : (2 * rightY)
 
-      if (right > 250) right = 256
-      if (right < -250) right = -256
+    this.server.on('accelerometer', ({ x, y, z }) => {
+      console.log('accelerometer', { x, y, z })
+    })
 
+    this.server.on('distance', ({ distance }) => {
+      this.distances.push(distance)
+      console.log('distance', median(this.distances))
+      this.go = median(this.distances) > 30
+      if (size(this.distances) > 5) {
+        this.distances.shift()
+      }
+    })
+
+    this.redLine.on('data', (direction) => {
+      if (!isNumber(direction)) return
+      this.direction = direction * 2
+    })
+
+    this.snesController.on('data', ({ dpad, buttons }) => {
+      const { horizontal, vertical } = dpad
+      const { l, r, b } = buttons
+
+      let left = parseInt(100 * (-1 * this.direction))
+      let right = parseInt(100 * (this.direction))
+      left += 100
+      right += 100
       this.server.send('move', { left, right })
+
+      // let left = 0
+      // let right = 0
+      // if (l) {
+      //   left = -255
+      //   right = -255
+      // }
+      //
+      // if (r) {
+      //   left = 255
+      //   right = 255
+      // }
+      //
+      // if (horizontal < 0) {
+      //   left -= 255
+      //   right += 255
+      // }
+      //
+      // if (horizontal > 0) {
+      //   left += 255
+      //   right -= 255
+      // }
+      //
+      // if (b) {
+      //   left = parseInt(left / 2)
+      //   right = parseInt(right / 2)
+      // }
+      //
+      // if (!this.go) {
+      //   left = min([-128, left])
+      //   right = min([-128, right])
+      // }
+      //
+      // this.server.send('move', { left, right })
     })
   }
 }
 
-new Command().run()
+new Command({ argv: process.argv }).run()
